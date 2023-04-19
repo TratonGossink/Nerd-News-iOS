1d1f15d778e728347066ba5b404a1dd564f11be1
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2023-04-19 12:12:40 -0500

    Initial Commit

---
+++ Nerd News iOS/Nerd_News_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nerd_News_iOSApp.swift
+//  Nerd News iOS
+//
+//  Created by Traton Gossink on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Nerd_News_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
